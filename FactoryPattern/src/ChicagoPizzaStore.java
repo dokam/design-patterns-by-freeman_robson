ce42a1717ff2ce751469bccf4c0603fadb961b49
Scologic <48550922+scologicdev@users.noreply.github.com>
2021-02-01 15:53:02 +0000

    factory method

---
+++ FactoryPattern/src/ChicagoPizzaStore.java
@@ -0,0 +1,6 @@
+public class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        return new ChicagoStyleCheesePizza();
+    }
+}
---
+++ FactoryPattern/src/NYPizzaStore.java
@@ -0,0 +1,6 @@
+public class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        return new NYStyleCheesePizza();
+    }
+}
